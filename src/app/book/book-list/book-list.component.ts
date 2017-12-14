d6e36dd9a0f7c7adcb8f751470f536790148794c
iw.jhun <dlfdnd0725@gmail.com>
2017-12-14 11:15:32 +0900

    Book List : update
    * Server 통신 error 시에 modal 장애 layer 추가

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -5,91 +5,91 @@ import {Subscription} from "rxjs/Subscription";
 import {RestApiService} from "../../services/RestApiService";
 
 @Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+    selector: 'app-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit, OnDestroy {
-  DATE_FORMAT = 'yyyy.MM.dd HH:mm:ss';
-  TITLE: string = 'Book Manager';
+    DATE_FORMAT = 'yyyy.MM.dd HH:mm:ss';
+    TITLE: string = 'Book Manager';
 
 
-  bookModel: Book[];
-  nowPage: number;
-  totalPage: number;
-  maxPage: number;
-  pagingRange;
+    bookModel: Book[];
+    nowPage: number;
+    totalPage: number;
+    maxPage: number;
+    pagingRange;
 
-  private subscribe: Subscription;
+    modalBoolean: boolean;
+    private subscribe: Subscription;
 
 
-  constructor(private restApiService: RestApiService) {
-    // this.setFirstPageNumb();
-    this.nowPage = 0;
-    this.maxPage = 9;
-    // this.onLoad(this.nowPage);
-  }
-
-  ngOnInit() {
-    // Observable.timer(timer, period);
-    // 5 seconds loop
-    this.subscribe = Observable.timer(0, 5000).subscribe(x => this.onLoad(this.nowPage), err => console.log(`err : ${{err}}`));
-  }
-
-  ngOnDestroy(): void {
-    this.subscribe.unsubscribe();
-  }
-
-  onLoad(nowPage: number) {
-    this.restApiService.getBookPage(nowPage).subscribe(
-      result => (
-        this.bookModel = result['data'],
-          this.totalPage = result['totalPage'],
-          this.pagingMaker(this.totalPage)
-      ),
-      error => console.error(`May be Server is Die.`)
-    )
-  };
-
-  clearAll() {
-    this.bookModel = null;
-  };
-
-  alertEndPage() {
-    alert('Page End');
-  }
-
-  pageUp() {
-    if (this.nowPage + 1 >= this.totalPage) {
-      this.alertEndPage();
-      return;
+    constructor(private restApiService: RestApiService) {
+        // this.setFirstPageNumb();
+        this.modalBoolean = false;
+        this.nowPage = 0;
+        this.maxPage = 9;
     }
-    this.nowPage++;
-    this.onLoad(this.nowPage);
-  }
 
-  pageDown() {
-    if (this.nowPage - 1 < 0) {
-      this.alertEndPage();
-      return;
+    ngOnInit() {
+        // Observable.timer(timer, period);
+        // 5 seconds loop
+        this.subscribe = Observable.timer(0, 5000).subscribe(x => this.onLoad(this.nowPage), err => console.log(`err : ${{err}}`));
     }
-    this.nowPage--;
-    this.onLoad(this.nowPage);
-  }
 
-  onClickPaging(numb: number) {
-    this.nowPage = numb;
-    this.onLoad(this.nowPage);
-  }
-
-  private pagingMaker(total: number) {
-
-    const range = Array();
-
-    for (let i = 0; i < total; i++) {
-      range.push(i);
+    ngOnDestroy(): void {
+        this.subscribe.unsubscribe();
     }
-    this.pagingRange = range;
-  }
+
+    onLoad(nowPage: number) {
+        this.restApiService.getBookPage(nowPage).subscribe(
+            result => (
+                this.bookModel = result['data'],
+                    this.totalPage = result['totalPage'],
+                    this.pagingMaker(this.totalPage),
+                    this.modalBoolean = false
+            ),
+            error => (console.error(`May be Server is Die.`), this.modalBoolean = true)
+        )
+    };
+
+    clearAll() {
+        this.bookModel = null;
+    };
+
+    alertEndPage() {
+        alert('Page End');
+    }
+
+    pageUp() {
+        if (this.nowPage + 1 >= this.totalPage) {
+            this.alertEndPage();
+            return;
+        }
+        this.nowPage++;
+        this.onLoad(this.nowPage);
+    }
+
+    pageDown() {
+        if (this.nowPage - 1 < 0) {
+            this.alertEndPage();
+            return;
+        }
+        this.nowPage--;
+        this.onLoad(this.nowPage);
+    }
+
+    onClickPaging(numb: number) {
+        this.nowPage = numb;
+        this.onLoad(this.nowPage);
+    }
+
+    private pagingMaker(total: number) {
+        const range = Array();
+        for (let i = 0; i < total; i++) {
+            range.push(i);
+        }
+        this.pagingRange = range;
+    }
 }
 
