b64cacd5f69d2e950b3542e3862a714f0a32cfa3
glqdlt <dlfdnd0725@gmail.com>
2018-01-09 15:02:33 +0900

    Home Components 수정

--- src/app/presentation/home/home-component.ts
+++ src/app/presentation/home/home-component.ts
@@ -7,13 +7,8 @@ import {SpinnerService} from "../../services/SpinnerService";
     styleUrls: ['./home-component.css']
 })
 export class HomeComponentComponent implements OnInit {
-    p: number = 1;
-    collection: any[];
 
     constructor(private spinnerService: SpinnerService) {
-
-        this.collection = new Array();
-        for(let i =0; i< 900; i++) this.collection.push(i);
     }
 
     ngOnInit() {
