a750341f39a8e246e109da266de4df3b3dde5f11
glqdlt <dlfdnd0725@gmail.com>
2018-01-26 17:02:13 +0900

    Detail에서 다운로드 구현

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,13 +1,12 @@
 import {NgModule} from "@angular/core";
 import {CardComponent} from "./card/card.component";
-import {BreadCrumb} from "./bread-crumb/bread-crumb";
 import {CommonModule} from "@angular/common";
 
 @NgModule({
 
     imports :[CommonModule],
-    exports : [CardComponent,BreadCrumb],
-    declarations : [CardComponent, BreadCrumb]
+    exports : [CardComponent],
+    declarations : [CardComponent]
 
 })
 export class ShareModule{
